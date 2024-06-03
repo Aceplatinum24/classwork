323f85dce8e56ff7c686bbd1f43b7b28071f1cf6
Aceplatinum24 <orgeron_j@yahoo.com>
2024-06-03 09:24:35 -0400

    Add files via upload

---
+++ 2b_operators.py
@@ -0,0 +1,56 @@
+## 1
+## Try this example
+
+# x = 13 / 5
+# print(x, "is the result of 13 / 5 using regular division.")
+
+# y = 13 // 5
+# print(y, "is the result of 13 // 5 sometimes called floor division.")
+
+# z = 13 % 5
+# print(z, "is the result of 13 % 5 sometimes called modulus or the remainder.")
+
+# print(f"So 13 divided by 5 gives you {x};")
+# print(f"Using floor division (13 // 5) drops the remainder giving you {y}.")
+# print(f"The modulus operator (%) gives you only the remainder of")
+# print(f"the operation ({z}).")
+# print(f"This represents 3 fifths or 3/5, which equals {z/5}")
+
+# a = 2 ** 5
+# print(a, "is the result of 2 ** 5, or 2 to the 5th power.")
+# print("5 is the exponent, so this can be called exponentiation.")
+# print("The (**) operator gives 2 to the power of 5 or 2*2*2*2*2")
+
+## 2
+## Now let's incorporate some inputs.
+## Ask the user for two numbers.
+## Display "If you divided ___ by ___, then you
+## would get ____ with a remainder of ___".
+
+
+## 2a
+## Here's a real life example:
+# pieces_of_candy = 23
+# people = 5
+
+# candy_per_person = pieces_of_candy // people
+# leftover = pieces_of_candy % people
+# print(f"If you had {pieces_of_candy} pieces of candy,")
+# print(f"and you wanted to share it with {people} people,")
+# print(f"each person would get {candy_per_person} pieces,")
+# print(f"and there would be {leftover} leftover.")
+
+
+## 3
+## Calculating an exponent
+## Ask the user for two numbers.
+## Display "If you computed ___ to the power of ___, then you would get ___."
+## (Hint: What is the operator for exponent in Python?
+##  It may not be what you think.)
+
+
+## 4
+## Operators that may not do what you expect
+## Based on question #1, does % calculate a percentage? (Answer: No)
+## Based on question #2, does ^ calculate an exponent? (Answer: No)
+
